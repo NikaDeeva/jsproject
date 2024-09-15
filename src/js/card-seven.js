0409c7145107ef32fd5099454dc5536491de3aa6
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-09-15 16:44:25 +0300

    football done

---
+++ src/js/card-seven.js
@@ -0,0 +1,22 @@
+const field = document.getElementById('footballField');
+const ball = document.getElementById('ball');
+
+field.addEventListener('click', event => {
+  const fieldWidth = field.offsetWidth;
+  const fieldHeight = field.offsetHeight;
+  const ballWidth = ball.offsetWidth;
+  const ballHeight = ball.offsetHeight;
+
+  let clickX = event.clientX - field.offsetLeft;
+  let clickY = event.clientY - field.offsetTop;
+
+  const minX = ballWidth / 2;
+  const minY = ballHeight / 2;
+  const maxX = fieldWidth - ballWidth / 2;
+  const maxY = fieldHeight - ballHeight / 2;
+
+  const ballX = Math.min(Math.max(clickX, minX), maxX);
+  const ballY = Math.min(Math.max(clickY, minY), maxY);
+
+  ball.style.transform = `translate(${ballX - minX}px, ${ballY - minY}px)`;
+});
