39f559426d2f59d78d60b3b6590b2508b119b7e4
Davidmitseruk <davidmitseruk@gmail.com>
2024-09-19 15:22:58 +0200

    lololo

--- src/js/card-nine.js
+++ src/js/card-nine.js
@@ -1,2 +1,21 @@
 
-  
+let currentSlide = 0;
+
+const slides = document.querySelectorAll('.slides')
+const prevBtn = document.querySelector('#prevBtn')
+const nextBtn = document.querySelector('#nextBtn')
+
+function updateButtons (){
+    prevBtn.disabled = currentSlide === 0;
+    nextBtn.disabled = currentSlide === slides.length - 1;
+
+}
+function moveSlides (number){
+    slides[currentSlide].classList.remove('active');
+    currentSlide += number;
+    slides[currentSlide].classList.add('active');
+    updateButtons();
+    
+}
+updateButtons()
+
