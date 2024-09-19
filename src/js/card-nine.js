ffdb50c53351c85da02db42a0ae93689cca712f0
Yulia Pedchenko <pedcenkoulia2@gmail.com>
2024-09-19 17:01:14 +0300

    decided prblem

--- src/js/card-nine.js
+++ src/js/card-nine.js
@@ -1,21 +1,20 @@
 
 let currentSlide = 0;
+const slides = document.querySelectorAll('.slides');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
-const slides = document.querySelectorAll('.slides')
-const prevBtn = document.querySelector('#prevBtn')
-const nextBtn = document.querySelector('#nextBtn')
-
-function updateButtons (){
+function updateButtons(){
     prevBtn.disabled = currentSlide === 0;
     nextBtn.disabled = currentSlide === slides.length - 1;
 
 }
-function moveSlides (number){
+function moveSlides(number){
     slides[currentSlide].classList.remove('active');
     currentSlide += number;
     slides[currentSlide].classList.add('active');
     updateButtons();
     
 }
-updateButtons()
+updateButtons();
 
